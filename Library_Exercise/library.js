fa164c7673ae2f1a1e560bc658be6a217a93bc71
H <hilary.dupont@gmail.com>
2018-01-23 16:47:09 -0500

    push
    push

---
+++ Library_Exercise/library.js
@@ -0,0 +1,63 @@
+
+function setup(){
+  createCanvas(1065, 600, WEBGL);
+}
+
+function draw(){
+  background(250);
+  
+
+
+  translate(-240, -100, 0);
+  normalMaterial();
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  var dirX = (mouseX / width - 0.5) * 2;
+  var dirY = (mouseY / height - 0.5) * 2;
+  directionalLight(250, 250, 250, -dirX, -dirY, 0.25);
+  torus(70, 70);;
+  pop();
+
+  
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cone(70, 70);;
+  
+  
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(140, 140);
+  pop();
+  
+  translate(-240 * 2, 200, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(70, 70);;
+  pop();
+  
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cone(70, 70);;
+  pop();
+  
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cone(70, 70);;
+  pop();
+}
