84285012f93e9097dee660b70e6c0d122901ca28
H <hilary.dupont@gmail.com>
2018-01-25 14:54:13 -0500

    push
    testing for portfolio site

--- jtest/jtest.js
+++ jtest/jtest.js
@@ -1,68 +1,84 @@
-
-
-$(document).ready(function(){
-    $(".chicken").draggable();
-    });
-
-
-$(document).ready(function(){
-    $(".salad").draggable();
-    });
-/*
-$(document).ready(function() {
-    $('div').click(function() {
-        $(this).fadeOut('fast');
-    });
+$(function() {  
+    $('.title').click(function(){
+  $(this).toggleClass('clicked');
 });
-
-
-
-
-$(function() {
-
-  $(window).scroll(function() {
-
-    var position = $(window).scrollTop();
-
-    if(position > 0 && position < 600) {
-      //$('body').css('background', 'white');
-      $('.block1').css({opacity: '1'});
-      $('.chicken').css({opacity:"1"});
-
-     
-
-    } else if (position > 600 && position < 1200 ) {
-
-      // $('body').css('background', 'blue');
-      $('.block1').css({opacity: '1'});
-      
-      $('chicken').css({opacity:"1"});
-
-    
-
-    } else if (position > 1200 && position < 1800) {
-
-      // $('body').css('background', 'red');
-   
-      $('.block1').css({ opacity: '1' });
-
-      $('.chicken').css({opacity:'1' });
-
-     
-
-    } else if (position > 1800 && position < 2400) {
-      // $('body').css('background', 'yellow');
-      $('chicken');
-
-    } else if (position > 2400 && position < 3000) {		  		
-      // $('body').css('background', 'white');
-      $('.block1').css({ opacity: '1'});
-    
-    }
-
   });
 
-});  
-*/
 
+//Tells the circle to bounce off walls and change color:
 
+(function ($, window, undefined) {
+  $.fn.marqueeify = function (options) {
+    var settings = $.extend({
+      horizontal: true,
+      vertical: true,
+      speed: 100, // In pixels per second
+      container: $(this).parent(),
+      bumpEdge: function () {}
+    }, options);
+    
+    return this.each(function () {
+      var containerWidth, containerHeight, elWidth, elHeight, move, getSizes,
+        $el = $(this);
+
+      getSizes = function () {
+        containerWidth = settings.container.outerWidth();
+        containerHeight = settings.container.outerHeight();
+        elWidth = $el.outerWidth();
+        elHeight = $el.outerHeight();
+      };
+
+      move = {
+        right: function () {
+          $el.animate({left: (containerWidth - elWidth)}, {duration: ((containerWidth/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
+            settings.bumpEdge();
+            move.left();
+          }});
+        },
+        left: function () {
+          $el.animate({left: 0}, {duration: ((containerWidth/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
+            settings.bumpEdge();
+            move.right();
+          }});
+        },
+        down: function () {
+          $el.animate({top: (containerHeight - elHeight)}, {duration: ((containerHeight/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
+            settings.bumpEdge();
+            move.up();
+          }});
+        },
+        up: function () {
+          $el.animate({top: 0}, {duration: ((containerHeight/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
+            settings.bumpEdge();
+            move.down();
+          }});
+        }
+      };
+
+      getSizes();
+
+      if (settings.horizontal) {
+        move.right();
+      }
+      if (settings.vertical) {
+        move.down();
+      }
+
+      // Make that shit responsive!
+      $(window).resize( function() {
+        getSizes();
+      });
+    });
+  };
+})(jQuery, window);
+
+$(document).ready( function() {
+
+  $('.marquee').marqueeify({
+    speed: 100,
+    bumpEdge: function () {
+      var newColor = "hsl(" + Math.floor(Math.random()*360) + ", 100%, 50%)";
+      $('.marquee .logo').css('fill', newColor);
+    }
+  });
+});
